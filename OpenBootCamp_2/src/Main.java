f1bdf1353d525b599879a061f1a5dd799185cacb
yl-test1 <yehudilara@hotmail.com>
2022-07-20 16:19:34 -0400

    Entrega ejercicios tema 3

---
+++ OpenBootCamp_2/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Crear un bucle que permite concatenar textos y imprima el resultado final por consola.
+         * concatenar nombres
+         * los textos pueden venir de un array string
+         * String[] nombres = { "", "", "", "" };
+         */
+        // Variable que contendrá la frase
+        String a = "";
+        // Lista de tipo String con palabras
+        String[] nombres = { "Hola", "Open", "Boot", "Camp" };
+        // Loop forEach para sacar las palabras
+        for (String nombre: nombres) {
+            // Se declara la variable a y le agregamos las palabras
+            a = a +" "+ nombre;
+
+        }
+        // MoWstramos variable.
+        System.out.println( a  );
+
+    }
+}
